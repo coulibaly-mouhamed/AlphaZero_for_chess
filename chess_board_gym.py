2eae772871786df6b21cf9fa26f8f55cdb105bbb
coulibaly-mouhamed <63076571+coulibaly-mouhamed@users.noreply.github.com>
2022-12-23 20:52:15 +0100

    change_scripts_to_use_gym_env

---
+++ chess_board_gym.py
@@ -0,0 +1,71 @@
+from email import policy
+import gym 
+import gym_chess
+import random 
+import chess
+from gym_chess.alphazero import BoardEncoding
+from gym_chess.alphazero import MoveEncoding
+from alphazero_nets import *
+import numpy as np 
+'''
+In this script we will create environnement for the chess game 
+'''
+
+def random_model_choice(board,alphazero):
+    policy, value = alphazero.forward(board)
+    #Return the id of the action with the highest policy
+    # Choose a random id from the list of ids with the highest policy
+    id_2 = random.choice(np.argwhere(policy == np.amax(policy)).flatten())
+    #id_action = np.argmax(policy.detach().numpy())
+    return id_2
+
+
+
+class game():
+    
+    def __init__(self,env,player_turn):
+        self.env = env
+        self.player_turn = player_turn # 1 for white and -1 for black
+        self.board = env.reset()
+        self.neural_net = Alphazero_net()
+        
+    def play(self,n_coups):
+        done = False
+        for i in range(n_coups):
+            if self.player_turn == 1:
+                action = random.choice(self.env.legal_actions)
+                self.board, reward, done, info = self.env.step(action)
+            else:
+                found_action = False
+                count = 0
+                while (found_action and count < 100000):
+                    action = random_model_choice(self.board,self.neural_net)
+                    try:
+                        self.board.push(self.env.action_to_move(action))
+                        self.board, reward, done, info = self.env.step(action)
+                        count += 1
+                        found_action = True
+                    except:
+                        count += 1
+                        print("count = ",count)   
+                
+            self.player_turn *= -1
+            
+            if done:
+                print("Game Over")
+                print("Reward", reward)
+                print('Winner is : ',-self.player_turn)
+                print(self.env.render(mode='unicode'))
+                break
+            
+        return self.env #,self.board, reward, done, info
+    
+if __name__ == '__main__':
+    env = gym.make('Chess-v0')
+    env = BoardEncoding(env,history_length=1)
+    env = MoveEncoding(env)
+    env.reset()
+    game = game(env,1)
+    game.play(50)
+    print(env.render(mode='unicode'))
+    
--- mcts.py
+++ mcts.py
@@ -2,3 +2,61 @@
 Implement a Monte Carlo Tree Search algorithm for the game of chess.
 '''
 import torch
+
+class Node:
+    
+    def __init__(self,state,prior_probability,player_turn):
+        '''
+        Node class that describes a node in the MCTS tree. It contains:
+            -state: the state of the board
+                *type : torch.tensor of size (22,8,8)
+            -Prior_probability: prior probbabilty for this stata predicted by the neural network for example for a given root node the children nodes with highest prior probability will be more likely to be explored
+                *Prior probability has the same type as the policy head output of the neural network (torch.tensor of size 4672=73*8*8)
+            -player_turn: the player turn
+                *type : int
+            -children: a dictionnary of children nodes
+                *type : dict
+            -value: the value of the node
+                *type :float between -1 and 1
+            -visit_count: the number of times the node has been visited
+                *type : int       
+        '''
+        self.state = state
+        self.prior_probability = prior_probability
+        self.player_turn = player_turn
+        self.children = {}
+        self.value = 0
+        self.visit_count = 0
+    
+    def expand(self,prior_probability):
+        '''
+        Expand the node by adding a child node to the node
+         prior_probability: tensor of size (4672= 73*8*8) containing the prior probability for each possible move
+        '''
+        n_actions = 4672 # 8*8*73 number of possible actions
+        #Loop over prior probability to find the moves that have a non zero probability
+        for id_action in range (n_actions):
+            if prior_probability[id_action] != 0:
+                #State of the board coming from the action
+                state_child = 
+                
+        
+        pass
+
+class Tree:
+    '''
+    Tree of  possibilies in chess game 
+    '''
+    
+    def __init__(self, root):
+        self.root = root
+        self.children = []
+        self.visits = 0
+        self.value = 0
+        
+    def expand(self, children):
+        self.children = children
+        
+    def evaluate(self,neural_net):
+        self.value = neural_net(self.root)
+        self.visits += 1
